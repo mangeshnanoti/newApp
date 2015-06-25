49f8f4cc54b173a2972cb0b203f32b3d56714737
mangeshnanoti <mangeshnanoti@gmail.com>
2015-06-26 01:19:00 +0530

    Counter variables working. Db working.
    The counter is working properly. The only thing needed now is a proper
    tact to store data.

--- client/newApp.js
+++ client/newApp.js
@@ -1,34 +1,40 @@
 counter1 = 0;
 counter2 = 0;
 c1=0;
-c2=0;
+c2=0; 
+x=0;
+y=0;
+
+
 maleCount = 0;
 femaleCount = 0;
-clk = 0
-//maleCount = cnt[0].maleCount;
-//femaleCount = cnt[0].femaleCount;
+ 
+clk = 0;
+clk1 = 0;
+
 
 if (Meteor.isClient) {
-  Session.setDefault('counter1', 0);
-  Session.setDefault('counter2', 0);
+cnt = Posts.findOne();
+maleCount = cnt.maleCount;
+femaleCount = cnt.femaleCount;
+counter1 = maleCount;
+counter2 = femaleCount;
+
+
+  Session.setDefault('counter1', counter1);
+  Session.setDefault('counter2', counter2);
   Session.set('maleCount', maleCount);
   Session.set('femaleCount', femaleCount);
-    rec = post.find('1').fetch();
-    console.log(rec[0].maleCount);
-    //console.log(rec[1].maleCount);
-    maleCount = rec[0].maleCount;
-    console.log(rec[0].maleCount);
   Template.hello.helpers({
 
     counter1: function () {
-      c1 = Session.get('counter1');
-    
-      return c1;
-                         
-    console.log(c1);
-    
 
-    },
+      c1 = Session.get('counter1');
+      return c1;
+      console.log(mlcnt);
+
+      },
+
     counter2: function () {
     
       c2 = Session.get('counter2');
@@ -44,51 +50,81 @@ if (Meteor.isClient) {
     femaleCount: function() {
     fc = Session.get('femaleCount');
     return fc;
+    },
+
+    Posts: function() {
+      return Posts;
+
     }
+
   });
 
     Template.hello.events({
     'click #maleb': function () {
-
+      //getCount1();
+      
       // increment the counter when button is clicked
       Session.set('counter1', Session.get('counter1') + 1);
       clk++;
-      console.log(clk);
+      console.log("Number of Clicks - " + clk);
+      maleCount++;
+
+      Posts.update({_id:1} , {$set: { 'maleCount' : clk } });
       
     },
 
     'click #femaleb': function () {
       // increment the counter when button is clicked
+      
       Session.set('counter2', Session.get('counter2') + 1);
+      clk1++;
+      femaleCount++;
+      Posts.update({_id:1} , {$set: { 'femaleCount' : clk1 } } );
     },
 
     'click #malec': function () {
       // increment the counter when button is clicked
+      
       Session.set('counter1', Session.get('counter1') - 1);
+      clk--;
+
+      Posts.update({_id:"1"} , {$set: { 'maleCount' : clk } } );
 
     },
 
     'click #femalec': function () {
+      
       // increment the counter when button is clicked
       Session.set('counter2', Session.get('counter2') - 1);
+      clk1--;
+      Posts.update({_id:"1"} , {$set: { 'femaleCount' : clk1 } });
     },
 
     'mouseleave #maleb' : function() {
+    //rec = getCount();
+
+    //Posts.update({_id:"1"} , {$set: {'maleCount' : clk}} );
+    //clk = clk+maleCount;
+    //Posts.update({_id:"0"} , {$set: {'maleCount' : clk}});
+
     Session.set('maleCount', maleCount);
-    post.update({_id:"1"} , {$set: {'maleCount' : clk}});
     //Session.set('maleCount' , clk);
+
     },
                           
     'mouseleave #femaleb' : function() {
     Session.set('femaleCount', fc);
+    //Posts.update({_id:"1"} , {$set: {'femaleCount' : clk1 } } );
     },
                           
     'mouseleave #malec' : function() {
     Session.set('maleCount', mc);
+    //Posts.update({_id:"1"} , {$set: {'maleCount' : clk } } );
     },
                           
     'mouseleave #femalec' : function() {
     Session.set('femaleCount', fc);
+    //Posts.update({_id:"1"} , {$set: {'femaleCount' : clk1 } } );
     }
 
 
@@ -96,9 +132,4 @@ if (Meteor.isClient) {
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    
-  });
-}
 
